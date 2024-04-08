c37a18e1198d8fe74962eb9ec115981dffc8e8f4
Abinash <abinashbehera9898@gmail.com>
2024-04-09 01:00:33 +0530

    Comparision between two number

---
+++ IF_ELSE/TwoNoCompare.java
@@ -0,0 +1,19 @@
+package IF_ELSE;
+
+public class TwoNoCompare {
+    public static void main(String[] args) {
+        int a = 2;//small
+        int b =3;//big
+
+        // we want to know which number is bigger or smaller
+
+        if (a<b) {
+            System.out.println(b+"is bigger than "+a);
+        }
+        else{
+            System.out.println(a+"is bigger than"+b);
+        }
+
+        //Ans == 3 is bigger than 2
+    }
+}
