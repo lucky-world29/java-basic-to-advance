22a07c88397b849f889adc3a1a0c76948c4ad9c2
Abinash <abinashbehera9898@gmail.com>
2024-04-18 02:12:14 +0530

    Stirng buffer and builder added

---
+++ String_StringBuffer_StringBuilder/BuilderTest.java
@@ -0,0 +1,7 @@
+public class BuilderTest {
+    public static void main(String[] args){  
+        StringBuilder builder=new StringBuilder("hello");  
+        builder.append("java");  
+        System.out.println(builder);  
+    }  
+}
