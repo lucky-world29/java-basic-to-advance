2bbffd7da9fec440d10904bd79900e98ac14df90
Abinash <abinashbehera9898@gmail.com>
2024-05-24 19:42:00 +0530

    .

---
+++ Random_practice/for_each.java
@@ -0,0 +1,20 @@
+package Random_practice;
+
+public class for_each {
+    String[] str1 = new String[5];
+    // str[0]="Abi";
+    // str[0]= "Ani";
+    // str[1]= "Sam";
+    // str[2]= "Joe";
+
+    String[] str2 = { "Ani", "Sam", " Joe" };
+
+    String[] strAr = new String[] { "Ani", "Sam", "Joe" }; // inline initialization
+
+    // String[] strAr = {"Ani", "Sam", "Joe" };
+
+    public static void main(String[] args) {
+        
+    }
+
+}
