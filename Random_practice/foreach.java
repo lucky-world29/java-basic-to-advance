ed2b21f34f22d239bb24dcc5b7a4a4f71f68bf4c
Abinash <abinashbehera9898@gmail.com>
2024-05-24 19:25:32 +0530

    .

---
+++ Random_practice/foreach.java
@@ -0,0 +1,11 @@
+package Random_practice;
+
+public class foreach {
+    String[] str = new String[5];
+    // str[0]="Abi";
+    str[0]= "Ani";  
+    str[1]= "Sam";  
+    str[2]= "Joe";
+    
+}
+
