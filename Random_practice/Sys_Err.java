ca84ed0c63d3d473874506a8f68fc0d418ae63e7
Abinash <abinashbehera9898@gmail.com>
2024-06-02 16:49:47 +0530

    .

--- Random_practice/Sys_Err.java
+++ Random_practice/Sys_Err.java
@@ -3,6 +3,14 @@ package Random_practice;
 public class Sys_Err {
     public static void main(String[] args) {
         System.err.println("Error message");
+        /*
+         * System.err.println() method to print an error message 
+         * "Error: Invalid input!" to the console, 
+         * and since it is a standard error stream, 
+         * it will be printed to the default console error output. 
+         * We can also assign System.err to a new PrintStream
+         *    object to redirect the standard error output to a file or another output stream.
+         */
         System.out.println('a');
     }
 }
