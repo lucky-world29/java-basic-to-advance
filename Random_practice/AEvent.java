a8116d0f021c8cd963c51b09d862fdedf90629e8
Abinash <abinashbehera9898@gmail.com>
2024-06-05 21:36:30 +0530

    .:.

--- Random_practice/AEvent.java
+++ Random_practice/AEvent.java
@@ -1,11 +1,9 @@
 package Random_practice;
-import java.awt.*;
-import java.awt.event.ActionListener;
 
-abstract class AEvent extends Frame implements ActionListener{
 
-    abstract int mm();
+public class AEvent {
+
     public static void main(String[] args) {
-        System.out.println("Hello");
+        
     }
-}       
+}
