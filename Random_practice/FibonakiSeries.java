c5fef02ce8b4e429b467d944293a4e5284072d27
Abinash <abinashbehera9898@gmail.com>
2024-06-06 15:27:46 +0300

    .

--- Random_practice/FibonakiSeries.java
+++ Random_practice/FibonakiSeries.java
@@ -2,7 +2,10 @@ package Random_practice;
 
 public class FibonakiSeries {
     public static void main(String[] args) {
-        int f1,f2=1;
-        
+        int f1=1,f2=1;
+        int fn;
+        int f = f1+f2 ;
+        System.out.println(f);
+
     }
 }
