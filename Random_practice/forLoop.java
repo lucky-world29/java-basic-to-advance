4b892fb146367fe8a957005268ec5b086f9c746d
Abinash <abinashbehera9898@gmail.com>
2024-06-11 19:27:00 +0300

    .

---
+++ Random_practice/forLoop.java
@@ -0,0 +1,24 @@
+package Random_practice;
+
+public class forLoop {
+    public static void main(String[] args) {
+        mm();
+        int jj=1;
+        while ( jj<5) {
+            System.out.println(
+                jj
+            );
+            continue;  //infinite time print 
+            ///jj ++;
+        }
+    }
+    public static void mm(){
+        for(int i=0;i<=5;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+    
+}
