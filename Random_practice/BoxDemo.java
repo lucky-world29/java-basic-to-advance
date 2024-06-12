6e891e71c544502cd03dec1d086e288ebc805061
Abinash <abinashbehera9898@gmail.com>
2024-06-12 16:05:15 +0530

    .

--- Random_practice/BoxDemo.java
+++ Random_practice/BoxDemo.java
@@ -1,26 +1,52 @@
+// This is still pending . Need to complete this code .......
+
+
+
 package Random_practice;
 
-class Box{
-    double width;
-    double height;
-    double depth;
+// class Box{
+//     double width;
+//     double height;
+//     double depth;
+// }
+
+class A{
+    int b;
+    int c;
 }
 
+class B{
+    public void innerB(){
+        A a = new A();
+        a.b=9;
+        a.c=12;
+    }
+}
+
+class C {
+
+}
 
 
 public class BoxDemo { 
 
-    public static void main(String[] args) {
-        Box mybox = new Box();
-        double vol;
+    // public static void main(String[] args) {
+    //     Box x = new Box();
+    //     double vol;
 
-        mybox.width = 10;
-        mybox.height = 20;
-        mybox.depth = 15;
+    //     x.width = 10;
+    //     x.height = 20;
+    //     x.depth = 15;
 
-        vol = mybox.width* mybox.height * mybox.depth;
+    //     vol = x.width* x.height * x.depth;
 
-        System.out.println("Vol is " +vol);
-    }
+    //     System.out.println("Vol is " +vol);
+    // }
     
+    public static void main(String[] args) {
+        B obj = new B();
+        obj.innerB();
+        BoxDemo obj1 = new BoxDemo();
+        
+    }
 }
