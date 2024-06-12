79448b39efe3c19b6e12d765b0255aa809a36ddd
Abinash <abinashbehera9898@gmail.com>
2024-06-12 22:09:38 +0530

    .

--- Random_practice/InnerClass.java
+++ Random_practice/InnerClass.java
@@ -10,5 +10,16 @@ public class InnerClass {
         InnerClass obj = new InnerClass();
         // A a = new A();
         // obj.A.meth();
+        InnerClass outerObject = new InnerClass();
+        InnerClass.A innerObject = outerObject.new A();
+        innerObject.meth();
+
+
+        /*
+         * Formula for calling InnerClass object ...===>> 
+         */
+        // OuterClass outerObject = new OuterClass();
+        // OuterClass.InnerClass innerObject = outerObject.new InnerClass();
     }
 }
+
