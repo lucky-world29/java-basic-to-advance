fd1d49b5f4313517b32830fe131fcb34ebe006d7
Abinash <abinashbehera9898@gmail.com>
2024-06-12 15:43:56 +0530

    .

--- Random_practice/Insertion.java
+++ Random_practice/Insertion.java
@@ -4,28 +4,27 @@ import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Insertion {
-    
- /**
- * @param args
- */
-public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in) ;
-    int x;
-    float y;
-    String z;
 
-    System.out.println("Enter the value of x");
-    x=sc.nextInt();
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x;
+        float y;
+        String z;
 
-    System.out.println("Enter the value of y");
-    y=sc.nextFloat();
+        System.out.println("Enter the value of x");
+        x = sc.nextInt();
 
-    System.out.println("Enter the value of z");
-    z=sc.nextLine();
+        System.out.println("Enter the value of y");
+        y = sc.nextFloat();
 
-    System.out.println(x+" "+y+" "+z);
+        System.out.println("Enter the value of z");
+        z = sc.nextLine();
 
- }
+        System.out.println(x + " " + y + " " + z);
 
+    }
 
 }
