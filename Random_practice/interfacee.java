86026211029781ffe9076d00617041b7e949b3b7
Abinash <abinashbehera9898@gmail.com>
2024-06-20 15:37:45 +0530

    .

---
+++ Random_practice/interfacee.java
@@ -0,0 +1,15 @@
+package Random_practice;
+
+public class interfacee {
+    public static void main(String[] args) {
+        
+    }
+}
+
+interface A{
+}
+interface B extends A{
+}
+
+class C implements A{
+}
