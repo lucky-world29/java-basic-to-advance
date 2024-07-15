80b4e821de405b7ae1d833c1d4eae8802a9c180f
Abinash <abinashbehera9898@gmail.com>
2024-07-16 00:32:39 +0530

    aa.java file added

---
+++ aa.java
@@ -0,0 +1,15 @@
+public class aa {
+    public static void main(String[] args) {
+        char ch ='\u0065';
+        int x = 1;
+        // System.out.println("Welcome");
+        // System.out.println("Wel\1come");
+        // System.out.println("Wel\2come");
+        // System.out.println("I\3U");
+        // System.out.println(ch + x);
+        System.out.println((char)x);
+        // System.out.println("wel\4come");
+        // System.out.println(ch);
+        // System.out.println(x);
+    }
+}
