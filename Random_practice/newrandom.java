cf58ef3fb7aab1d4fa3cb56c84f8dc34d5366a9a
Abinash <abinashbehera9898@gmail.com>
2024-07-30 11:32:23 +0530

    .

---
+++ Random_practice/newrandom.java
@@ -0,0 +1,7 @@
+package Random_practice;
+
+public class newrandom {
+    public static void main(String[] args) {
+        System.out.println("Hello there");
+    }
+}
---
+++ Random_practice/randomNumber.java
@@ -0,0 +1,9 @@
+package Random_practice;
+
+public class randomNumber {
+    public static void main(String[] args) {
+
+        int rand1 = (int) (Math.random() * 10) + 10102;
+        System.out.println(rand1);
+    }
+}
