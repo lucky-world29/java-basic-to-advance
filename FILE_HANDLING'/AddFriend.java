cf9afff18ad339ee220b85b8f74ac593130acb36
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-11-19 00:44:41 +0530

    AddFriend.java is modified

--- FILE_HANDLING'/AddFriend.java
+++ FILE_HANDLING'/AddFriend.java
@@ -1,6 +1,28 @@
+import java.io.File;
+import java.io.IOException;
 
 public class AddFriend {
     public static void main(String[] args) {
+        System.out.println("ji");
         
+        try {
+            // Calling the AA method
+            AA();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+
+    static void AA() throws IOException {
+        File file = new File("abc.txt");
+        
+        // Attempting to create the file if it does not exist
+        boolean fileCreated = file.createNewFile();
+        if (fileCreated) {
+            System.out.println("File created: " + file.exists());
+        } else {
+            System.out.println("File already exists: " + file.exists());
+        }
     }
 }
