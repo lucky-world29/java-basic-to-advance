d8cc51fcefd70cf138d68149419feae2107df47c
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-11-28 00:20:36 +0530

    WTH is error with practice.java

--- Z-Random_practice/practice.java
+++ Z-Random_practice/practice.java
@@ -1,6 +1,5 @@
 
 
-
 class a{
 
     a(){
@@ -51,3 +50,36 @@ class practice extends b {
     }
 
 }
+
+/*
+ class a {
+    a() {
+        System.out.println("In class a");
+    }
+
+    void f1() {
+        System.out.println("hello world");
+    }
+}
+    class b extends a {
+    b(int x) {
+        System.out.println("in class b" + x);
+     }
+   }
+
+   class practice extends b {
+    practice() {
+        super(40);
+        System.out.println("In class practice");
+    }
+}
+
+public static void main(String[] args) {
+    practice ob = new practice();
+    ob.f1();
+}
+
+
+ */
+
+ // FIND THE SOLUTION WHERE IS THE ERROR
