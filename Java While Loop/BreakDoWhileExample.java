5497945a0a5371d257354ec2f26d4ef2dffdb8eb
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-11-27 23:37:59 +0530

    modified java1.java and BreakDoWhileExample.java

--- Java While Loop/BreakDoWhileExample.java
+++ Java While Loop/BreakDoWhileExample.java
@@ -14,3 +14,4 @@ public class BreakDoWhileExample {
         }while(i<=10);  
     }  
 }
+
--- Java While Loop/java1.java
+++ Java While Loop/java1.java
@@ -1,5 +1,8 @@
 public class java1 {
     public static void main(String[] args) {
         System.out.println("HI");
+        boolean obj = true;
+        int a = 90;
+        System.out.println(obj);
     }
 }
