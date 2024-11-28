2c0305dfba4e3ad1b01642d1b24daf0d8e0c9b3e
ABINASH BEHERA <128899808+lucky-1-4-3@users.noreply.github.com>
2024-11-28 21:19:05 +0530

    Modified

---
+++ Ch 6 Introducing Classes/BoxDemo2.java
@@ -0,0 +1,15 @@
+class Box {
+    double width;
+    double height;
+    double depth;
+}
+
+public class BoxDemo2 {
+    Box mybox1 = new Box();
+    Box mybox2 = new Box();
+    double vol;
+
+    
+    
+
+}
